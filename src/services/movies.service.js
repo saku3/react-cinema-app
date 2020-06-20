4a471ebcc3cd1f9f8f259eead4fef82961a12ba9
sakurai <39reyskhky@gmail.com>
2020-06-20 14:28:06 +0900

    added details api service methods and actions

--- src/services/movies.service.js
+++ src/services/movies.service.js
@@ -13,3 +13,28 @@ export const SEARCH_API_URL = async (query) => {
   const response = await axios.get(`${REQUEST_URL}/search/movie?api_key=${API_KEY}&language=ja&query=${query}`);
   return response;
 };
+
+export const MOVIE_DETAILS_URL = async (id) => {
+  const response = await axios.get(`${REQUEST_URL}/movie/${id}?api_key=${API_KEY}&language=ja`);
+  return response;
+};
+
+export const MOVIE_CREDITS_URL = async (id) => {
+  const response = await axios.get(`${REQUEST_URL}/movie/${id}/credits?api_key=${API_KEY}&language=ja`);
+  return response;
+};
+
+export const MOVIE_IMAGES_URL = async (id) => {
+  const response = await axios.get(`${REQUEST_URL}/movie/${id}/images?api_key=${API_KEY}&language=ja&include_image_language=en`);
+  return response;
+};
+
+export const MOVIE_VIDEOS_URL = async (id) => {
+  const response = await axios.get(`${REQUEST_URL}/movie/${id}/videos?api_key=${API_KEY}&language=en`);
+  return response;
+};
+
+export const MOVIE_REVIEWS_URL = async (id, page = 1) => {
+  const response = await axios.get(`${REQUEST_URL}/movie/${id}/reviews?api_key=${API_KEY}&language=en&page=${page}`);
+  return response;
+};
