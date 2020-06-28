2b2337ecb7bbe0a756dea47c826138ff2813d3df
sakurai <39reyskhky@gmail.com>
2020-06-28 19:56:40 +0900

    created error component

--- src/components/content/details/Details.js
+++ src/components/content/details/Details.js
@@ -12,11 +12,20 @@ import Crew from './crew/Crew';
 import Media from './media/Media';
 import { movieDetails } from '../../../redux/actions/movies';
 import { IMAGE_URL } from '../../../services/movies.service';
+import Spinner from '../../spinner/Spinner';
 
 const Details = (props) => {
   const { movieDetails, movie } = props;
   const [details, setDetails] = useState();
+  const [loading, setLoading] = useState(false);
   const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   useEffect(() => {
     if (movie.length === 0) {
@@ -28,50 +37,54 @@ const Details = (props) => {
   }, [id, movie]);
   return (
     <>
-      {details && (
-        <div className="movie-container">
-          <div className="movie-bg" style={{ backgroundImage: `url(${IMAGE_URL}${details.backdrop_path})` }}></div>
-          <div className="movie-overlay"></div>
-          <div className="movie-details">
-            <div className="movie-image">
-              <img src={`${IMAGE_URL}${details.poster_path}`} alt="" />
-            </div>
-            <div className="movie-body">
-              <div className="movie-overview">
-                <div className="title">
-                  {details.title} <span>{details.release_date}</span>
+      {loading ? (
+        <Spinner />
+      ) : (
+        details && (
+          <div className="movie-container">
+            <div className="movie-bg" style={{ backgroundImage: `url(${IMAGE_URL}${details.backdrop_path})` }}></div>
+            <div className="movie-overlay"></div>
+            <div className="movie-details">
+              <div className="movie-image">
+                <img src={`${IMAGE_URL}${details.poster_path}`} alt="" />
+              </div>
+              <div className="movie-body">
+                <div className="movie-overview">
+                  <div className="title">
+                    {details.title} <span>{details.release_date}</span>
+                  </div>
+                  <div className="movie-genres">
+                    <ul className="genres">
+                      {details.genres.map((genre) => {
+                        return <li key={genre.id}>{genre.name}</li>;
+                      })}
+                    </ul>
+                  </div>
+                  <div className="rating">
+                    <Rating className="rating-stars" rating={details.vote_average} totalStars={10} />
+                    &nbsp;
+                    <span>{details.vote_average}</span>
+                    <p>({details.vote_count}) reviews</p>
+                  </div>
+                  <Tabs>
+                    <div label="Overview">
+                      <Overview />
+                    </div>
+                    <div label="Crew">
+                      <Crew />
+                    </div>
+                    <div label="Media">
+                      <Media />
+                    </div>
+                    <div label="Reviews">
+                      <Reviews />
+                    </div>
+                  </Tabs>
                 </div>
-                <div className="movie-genres">
-                  <ul className="genres">
-                    {details.genres.map((genre) => {
-                      return <li key={genre.id}>{genre.name}</li>;
-                    })}
-                  </ul>
-                </div>
-                <div className="rating">
-                  <Rating className="rating-stars" rating={details.vote_average} totalStars={10} />
-                  &nbsp;
-                  <span>{details.vote_average}</span>
-                  <p>({details.vote_count}) reviews</p>
-                </div>
-                <Tabs>
-                  <div label="Overview">
-                    <Overview />
-                  </div>
-                  <div label="Crew">
-                    <Crew />
-                  </div>
-                  <div label="Media">
-                    <Media />
-                  </div>
-                  <div label="Reviews">
-                    <Reviews />
-                  </div>
-                </Tabs>
               </div>
             </div>
           </div>
-        </div>
+        )
       )}
     </>
   );
