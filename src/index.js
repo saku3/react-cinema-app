2dfb1c37bdbd8aee8cdf68c4b149880722172f32
sakurai <39reyskhky@gmail.com>
2020-07-03 22:43:34 +0900

    setup sentry for error monitoring

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import store from './redux/store';
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    beforeBreadcrumb(breadcrumb, hint) {
+      return breadcrumb.category === 'ui.click' ? null : breadcrumb;
+    }
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
